cee84f7b18757b700f604ba541af50ae8f918257
Canchis12 <sebascriollo7t@gmail.com>
2025-11-12 15:25:49 -0500

    Subo el login

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+console.log("✅ Script cargado correctamente");
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+  
+    const usuario = document.getElementById("username").value.trim();
+    const contraseña = document.getElementById("password").value.trim();
+  
+    if (usuario === "Grupo6" && contraseña === "123456") {
+      alert("Bienvenidos 🍰");
+      window.location.href = "../Paginas/Inicio.html";
+    } else {
+      alert("Usuario o Contraseña Incorrecta ❌");
+    }
+  });
+  
